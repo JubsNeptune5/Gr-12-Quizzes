6a132b4e15464cf07798c88647111fe111badad3
laveh2107 <laveh2107@HRH-IC0027301.wrdsb.ca>
2017-10-12 11:34:38 -0400

    Library Oct 12

--- src/Quiz1.java
+++ src/Quiz1.java
@@ -4,18 +4,23 @@
  */
 
 /**
- *
+ *First Quiz of the year
  * @author laveh2107
  */
 public class Quiz1 {
-
+/**
+ * Program to recursivly display the string backwards
+ * @param word
+ * @return 
+ */
     public static String reverseString(String word) {
         //Return word when the word only has one letter
         if (word.length() == 1) {
             return word;
         }
-        //Make 
+        //Make a substring of the string including everything except for the first letter 
         String newWord =  word.substring(1);
+        //Return the substring recusively + the first letter to print out the letters in reverse order
         return reverseString(word.substring(1)) + word.charAt(0);
     }
 
